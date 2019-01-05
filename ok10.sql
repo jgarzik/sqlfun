41b3ade6577377f383732a5d9eea80b89298c2a2
Jeff Garzik <jeff@bloq.com>
2019-01-05 03:20:30 -0500

    Add more test data

---
+++ ok10.sql
@@ -0,0 +1,2 @@
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
--- ok4.sql
+++ ok4.sql
@@ -1,1 +1,1 @@
-SELECT employee_id FROM employees WHERE job_id IS NULL;
+SELECT employee_id FROM employees WHERE job_id IS NULL order by employee_id;
